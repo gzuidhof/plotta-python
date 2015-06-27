5f410124e439ba5795335b3e0159eb1421e3ba52
Guido <me@guido.io>
2015-06-27 02:15:40 +0200

    Package setup definition

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='plotta',
+    version='1.0.0a1',
+    install_requires=['unirest'],
+
+    description='Python wrapper for Plotta API',
+    url='https://github.com/gzuidhof/plotta-python',
+    license='MIT',
+    keywords='plot plotting plotta',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
